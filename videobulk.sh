c4c6baf8a1594aa7206b66031f9984e15b13bf2b
root <DavidAlacant@gmail.com>
2016-04-26 13:50:02 +0200

    Now it works

--- videobulk.sh
+++ videobulk.sh
@@ -3,17 +3,29 @@
 # Script to convert all video files from a directory (parameter) into mp4 format file
 
 # Sanity checks ?
-# ...
+if [ $# -eq 0 ]
+ then 
+	directory=$PWD
+  elif [ -d $1 ]
+	then
+	directory=$1
+  else
+	echo "sorry. $1 is not a directory"
+fi
+    
 
 # Main code
 
 ls -1 $1*.avi > videofiles
 
-while videofile
+while read videofile
 do
-	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
+	lengthname=${#videofile}	#Gets the length of the name
+	namefile=${videofile: -3}	#Gets the extension of the file
+	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc
+
 
 done < videofiles
 rm videofiles
+
+exit 0
