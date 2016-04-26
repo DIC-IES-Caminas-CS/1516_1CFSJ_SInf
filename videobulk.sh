61d9803da71e5a4076c4fccea811ec679a46c088
dirigarays <alu20907041h@ieselcaminas.org>
2016-04-26 15:02:20 +0200

    “26-04-2016”

--- videobulk.sh
+++ videobulk.sh
@@ -3,17 +3,28 @@
 # Script to convert all video files from a directory (parameter) into mp4 format file
 
 # Sanity checks ?
-# ...
+if [ $# -eq 0 ]
+ then 
+	directory=$PWD
+  elif [ -d $1 ]
+	then
+	directory=$1
+  else
+	echo "sorry. $1 is not a directory"
+fi
+    
 
 # Main code
 
 ls -1 $1*.avi > videofiles
 
-while videofile
+while read videofile
 do
 	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
+	namefile=${videofile: -3}
 	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
 
 done < videofiles
 rm videofiles
+
+exit 0
