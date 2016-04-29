49555f4814699049616f9c5fc21f0bcb6c0ef18c
david1707 <DavidAlacant@gmail.com>
2016-04-29 13:54:32 +0200

    Option to delete old file and 'finished project' message added.

--- videobulk.sh
+++ videobulk.sh
@@ -26,7 +26,12 @@ then
 		namefile=${videofile:0:lengthname-4}	#Gets just the name (without extension)
 		mencoder $videofile -o $namefile.mp4 -oac mp3lame -ovc lavc #Uses lavc codec
 
+		if [ $3 == "-rm" ]
+		then
+			rm $namefile.avi
+		fi
 
+		echo "File(s) saved."
 	done < videofiles
 	rm videofiles
 fi
@@ -39,7 +44,12 @@ then
 		namefile=${videofile:0:lengthname-4}	#Gets just the name (without extension)
 		mencoder $videofile -o $namefile.mp4 -oac mp3lame -ovc x264 #Uses x264 codec
 
+		if [ $3 == "-rm" ]
+		then
+			rm $namefile.avi
+		fi
 
+		echo "File(s) saved."
 	done < videofiles
 	rm videofiles
 fi
@@ -51,8 +61,13 @@ then
 		lengthname=${#videofile}	#Gets the length of the name
 		namefile=${videofile:0:lengthname-4}	#Gets just the name (without extension)
 		mencoder $videofile -o $namefile.mp4 -oac mp3lame -ovc frameno #Uses frameno codec
+		
+		if [ $3 == "-rm" ]
+		then
+			rm $namefile.avi
+		fi
 
-
+		echo "File(s) saved."
 	done < videofiles
 	rm videofiles
 fi
